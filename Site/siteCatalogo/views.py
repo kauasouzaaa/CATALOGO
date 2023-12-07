1425877272985c24680156356806997e8d95445c
kauasouzaaa <0000862890@senaimgaluno.com.br>
2023-12-07 19:43:36 -0300

    static criado

--- Site/siteCatalogo/views.py
+++ Site/siteCatalogo/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hey.</h1>')
+    return HttpResponse('<h1>Hey.</h1>'),
+
+def pagina(request):
+    return render(request, 'index.html')
