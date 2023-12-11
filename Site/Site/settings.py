629a3bce24b9ca657efe60b3b0a6cbdee78e4796
kauasouzaaa <0000862890@senaimgaluno.com.br>
2023-12-11 19:38:01 -0300

    banco de dados criado

--- Site/Site/settings.py
+++ Site/Site/settings.py
@@ -53,7 +53,11 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
--- Site/siteCatalogo/models.py
+++ Site/siteCatalogo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Site(models.Model):
+    nome_card = models.CharField(max_length=200)
+    description =  models.TextField()
+    path = models.TextField()
+    primeiro_texto = models.TextField()
+    segundo_texto = models.TextField()
+    alunos = models.IntegerField()
+    date_create = models.DateTimeField(default=datetime.now, blank =True)
