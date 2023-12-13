e0556c3a49b94037985a42849c4c5a30ba1af4e2
kauasouzaaa <0000862890@senaimgaluno.com.br>
2023-12-12 21:58:06 -0300

    É isso...

--- Site/siteCatalogo/views.py
+++ Site/siteCatalogo/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Hey.</h1>'),
+    return render(request, 'index.html')
 
 def pagina(request):
-    return render(request, 'index.html')
+   pass
